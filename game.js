d8916cc26330778788ef41c1af540e6bb8f9b7d9
Maksim Harnastayeu <maksimgornostaev197@gmail.com>
2025-01-17 15:56:10 +0300

    Add JS script to project

---
+++ game.js
@@ -0,0 +1,77 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswer = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    {
+        question: "2+5",
+        choice1: "7",
+        choice2: "-3",
+        choice3: "3",
+        choice4: "8",
+        answer: 1
+    },
+    {
+        question: "4 + 6/2",
+        choice1: "2",
+        choice2: "4",
+        choice3: "5",
+        choice4: "7",
+        answer: 4
+    },
+    {
+        question: "7*5 - 3 + 2*4",
+        choice1: "22",
+        choice2: "40",
+        choice3: "36",
+        choice4: "12",
+        answer: 2
+    }
+];
+
+const CORRECT_BONUS = 10;
+const MAX_QUESTION = 3;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [ ... questions];
+    getNewQuestion();
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTION){
+        return window.location.assign("/end.html");
+    }
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach((choice) => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswer = true;
+}
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", (e) => {
+        if(!acceptingAnswer) return;
+
+        acceptingAnswer = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+        getNewQuestion();
+    })
+})
+
+startGame();
+
