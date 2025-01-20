cc9549f9f0ddea17387c27b5848a231852536c2f
Maksim Harnastayeu <maksimgornostaev197@gmail.com>
2025-01-20 12:49:32 +0300

    Add Quiz End Page

--- game.js
+++ game.js
@@ -2,13 +2,13 @@ const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
-const progressBarFull = document.getElementById("progressBarFull")
+const progressBarFull = document.getElementById("progressBarFull");
 
 let currentQuestion = {};
-let acceptingAnswer = false;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let availableQuestions = [];
+let availableQuesions = [];
 
 let questions = [
     {
@@ -38,64 +38,64 @@ let questions = [
 ];
 
 const CORRECT_BONUS = 10;
-const MAX_QUESTION = 3;
+const MAX_QUESTIONS = 3;
 
 startGame = () => {
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = [ ... questions];
-    getNewQuestion();
+  questionCounter = 0;
+  score = 0;
+  availableQuesions = [...questions];
+  getNewQuestion();
 };
 
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTION){
-        return window.location.assign("/end.html");
-    }
-    questionCounter++;
-    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTION}`;
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
+    return window.location.assign("/end.html");
+  }
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-    progressBarFull.style.width = `${(questionCounter / MAX_QUESTION) * 100}%`;
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
+  question.innerText = currentQuestion.question;
 
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
 
-    choices.forEach((choice) => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
-    });
-
-    availableQuestions.splice(questionIndex, 1);
-    acceptingAnswer = true;
+  availableQuesions.splice(questionIndex, 1);
+  acceptingAnswers = true;
 };
 
-choices.forEach((choice) => {
-    choice.addEventListener("click", (e) => {
-        if(!acceptingAnswer) return;
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
 
-        acceptingAnswer = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
 
-        const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
-        if(classToApply === "correct"){
-            incrementScore(CORRECT_BONUS);
-        }
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
 
-        selectedChoice.parentElement.classList.add(classToApply);
+    selectedChoice.parentElement.classList.add(classToApply);
 
-        setTimeout( () => {
-            selectedChoice.parentElement.classList.remove(classToApply);
-            getNewQuestion();
-        }, 500);
-    });
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
 });
 
 incrementScore = num => {
-    score += num;
-    scoreText.innerText = score;
+  score += num;
+  scoreText.innerText = score;
 };
 
 startGame();
-
